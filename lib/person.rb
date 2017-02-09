2878cd039ec76157806a5f829c53371c6c32646e
Alexandra Roth <alroth8@gmail.com>
2017-02-08 20:43:40 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,73 @@
 # your code goes here
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+
+  def happiness=(num)
+    if num > 10
+      num = 10
+    elsif num < 0
+      num = 0
+    end
+    @happiness = num
+  end
+
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+
+  end
+
+  def clean?
+    hygiene > 7
+  end
+
+  def happy?
+    happiness > 7
+  end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    self.happiness += 1
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    [self, friend].each { |x| x.happiness += 3}
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    if topic == "politics"
+      [friend, self].each {|x| x.happiness -= 2}
+    return "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      [friend, self].each {|x| x.happiness += 1}
+      return "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+
+
+end
